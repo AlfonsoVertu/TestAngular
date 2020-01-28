2a4ac743c51da14688ab31f21e4781cf6b1c4761
AlfonsoVertu <alfonsovertu@gmail.com>
2020-01-28 15:14:55 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'applicami',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
   name = 'Angular';
+  porco = 'Dio';
 }
--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,8 +1,8 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  selector: 'Ciao',
+  template: `<h1><a href="www.libertaeazione.info/digitalsalerno/{{ name }}" title="{{ name }}">Benvenuto {{name}}!</a></h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
